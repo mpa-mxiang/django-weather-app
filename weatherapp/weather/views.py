bc28e7de05fdb776ea9ba877ab7f338de6ada496
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-09 00:35:32 -0400

    import load_dotenv to resolve hidden api key not work issue

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,21 +1,44 @@
 from django.shortcuts import render
-
+import os
+import requests 
+import datetime 
+from dotenv import load_dotenv
+load_dotenv()
 # Create your views here.
 def home(request):
     api_key = os.getenv('OPEN_WEATHER_API_KEY')  # Grab the API key from environment variables
+
     if 'city' in request.POST:
         city = request.POST['city']
     else:
-        city = 'New York'
+        city = 'Toronto'
+        
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-    PARMAS = {'units': 'metric'}
+    params = {'units': 'metric'}
+    
+    try:
+        response = requests.get(url, params=params)
+        data = response.json()
 
-    data = request.get(url, PARMAS).json()
-
-    describe = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
+        
+        # Check if 'weather' key exists in the response
+        if 'weather' in data:
+            describe = data['weather'][0]['description']
+            icon = data['weather'][0]['icon']
+            temp = data['main']['temp']
+        else:
+            # If 'weather' key is not present, handle the error
+            describe = 'Weather data not available'
+            icon = None
+            temp = 0
+        
+    except Exception as e:
+        # Handle any other exceptions (e.g., network issues)
+        describe = 'Error fetching data'
+        icon = None
+        temp = None
 
     day = datetime.date.today()
+    
 
-    return render(request, 'weather/index.html', {'description': describe, 'icon': icon, 'temp': temp, 'day' : day})
+    return render(request, 'weather/index.html', {'description': describe, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
