9560a327d3c3d1c95171d460349db68d7018f699
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-08 23:43:18 -0400

    Create a home view with adding openWeather API

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hi this is my django home page!")
+    api_key = os.getenv('OPEN_WEATHER_API_KEY')  # Grab the API key from environment variables
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        city = 'New York'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    PARMAS = {'units': 'metric'}
+    return render(request, 'weather/index.html')
--- weatherapp/weatherapp/settings.py
+++ weatherapp/weatherapp/settings.py
@@ -11,6 +11,15 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+# Load the .env file
+load_dotenv()
+
+# Access your API key from .env
+OPEN_WEATHER_API_KEY = os.getenv('OPEN_WEATHER_API_KEY')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
