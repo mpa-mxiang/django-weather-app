2529007dca36161ec8bd5f83a2387b98d450e411
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-09 22:56:11 -0400

    add static in setting and url

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.config import settings
+from django.config.urls.static import static
 
 urlpatterns = [
     path('', include('weather.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
