d833f9f5d3b3112d2762bca0807abc94e69a06e5
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-09 23:02:46 -0400

    fix static import in urls

--- weatherapp/weatherapp/urls.py
+++ weatherapp/weatherapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('', include('weather.urls')),
