f571aad6cc03bdbb00275de23744b489a633e440
Jiachun Xiang <jcxiang1230@gmail.com>
2024-09-09 23:41:47 -0400

    use google programmable search engine to grab city image

--- weatherapp/weather/views.py
+++ weatherapp/weather/views.py
@@ -6,14 +6,28 @@ from dotenv import load_dotenv
 load_dotenv()
 # Create your views here.
 def home(request):
-    api_key = os.getenv('OPEN_WEATHER_API_KEY')  # Grab the API key from environment variables
+    # Grab the API key from environment variables
+    weather_api_key = os.getenv('OPEN_WEATHER_API_KEY')  
+    search_api_key = os.getenv('SEARCH_ENGINE_API_KEY')
+    search_id = os.getenv('SEARCH_ENGINE_ID')
+
+    query = city + "1920x1080"
+    page = 1
+    start = (page-1) * 10 + 1
+    searchType = 'image'
+    city_url = f"https://www.googleapis.com/customsearch/v1?key={search_api_key}&cx={search_id}&q={query}&start={start}&searchType={searchType}&imgSize=xlarge"
+
+    data = request.request.get(city_url).json()
+    count = 1
+    search_items = data.get('items')
+    image_url = search_items[1]['link']
 
     if 'city' in request.POST:
         city = request.POST['city']
     else:
         city = 'Toronto'
         
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={weather_api_key}'
     params = {'units': 'metric'}
     
     try:
@@ -41,4 +55,4 @@ def home(request):
     day = datetime.date.today()
     
 
-    return render(request, 'weather/index.html', {'description': describe, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
+    return render(request, 'weather/index.html', {'description': describe, 'icon': icon, 'temp': temp, 'day': day, 'city': city, 'image_url': image_url})
